210a4c3259fb40fd5cf980f334c0b188dcdadd3b
saerom0 <jinxinsr@naver.com>
2023-05-03 19:55:53 +0900

    signup 페이지 완성, slideToggle, slideUp 등 메서드 사용

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,15 @@
 $(function () {
-	$('.gnb-include').load('/include/gnb.html');
-	$('.main-header-include').load('/include/main-header.html');
+	/* Include html Files */
+	$('.gnb-include').load('/component/gnb.html');
+	$('.main-header-include').load('/component/main-header.html');
+	$('.goods-detail-header-include').load('/component/goods-detail-header.html');
+
+	/* Category Accordion */
+	$('.category-accordion .detail').eq(0).show();
+	$('.category-accordion .title').on('click', function () {
+		$(this).next().stop().slideToggle();
+		$(this).siblings('.category-accordion .title').next().stop().slideUp();
+		$(this).siblings('.category-accordion .title').removeClass('active');
+		$(this).toggleClass('active');
+	});
 });

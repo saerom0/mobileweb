643d0c703269f55d165ec40a0bd0a41ec88d2534
saerom0 <jinxinsr@naver.com>
2023-05-06 22:27:23 +0900

    front page 작업완료

--- js/custom.js
+++ js/custom.js
@@ -26,57 +26,6 @@ $(function () {
 	$('.wish').on('click', function () {
 		$(this).toggleClass('active');
 	});
-	/* cart page */
-	/* title 길이 자르기*/
-	$('.cart-item-title span').text(function (_, text) {
-		return text.length > 50 ? text.substr(0, 50) + '...' : text;
-	});
-	/* 삭제버튼 이벤트 */
-	$('.selboxDirect').hide();
-	$('#selbox').on('change', function () {
-		if ($('#selbox').val() == 'direct') {
-			$('.selboxDirect').show();
-		} else {
-			$('.selboxDirect').hide();
-		}
-
-		/* month, date 연결 */
-		var month = new Date().getMonth() + 1;
-		var date = new Date().getDay() + 1;
-
-		$('.cart-item-info .delivery-date b').eq(0).text(month);
-		$('.cart-item-info .delivery-date b').eq(1).text(date);
-	});
-
-	/* Cart items 삭제버튼*/
-	$('.cart-items-header .btn-all-clear').on('click', function () {
-		$('.cart-item').hide();
-		$('.cart-items-summary .price').text('0원');
-		$('.cart-items').html(
-			'<p class="no-item">장바구니에 담긴 상품이 없습니다.</p>'
-		);
-	});
-	$('.cart-item-title .btn-clear').on('click', function () {
-		console.log('click');
-		$(this).parent().parent().hide();
-	});
-
-	/* cart-item의 수량버튼 */
-	$('.count-wrap button').on('click', function (e) {
-		e.preventDefault();
-		var $count = $(this).parent('.count-wrap').find('.input');
-		var now = parseInt($count.val());
-		var min = 1;
-		var max = 99;
-		var num = now;
-
-		if ($(this).hasClass('minus')) {
-			if (now > min) num = now - 1;
-		} else {
-			if (now < max) num = now + 1;
-		}
-		if (num != now) $count.val(num);
-	});
 
 	/* Front Slider */
 	$('.front-slider').slick({
@@ -101,8 +50,60 @@ $(function () {
 		return text.length > 24 ? text.substr(0, 24) + '...' : text;
 	});
 
-	/* Front footer btn-view-more toggle */
+	// Front footer btn-view-more toggle
 	$('.btn-view-more').on('click', function () {
 		$(this).parent().siblings().slideToggle();
+	});
+
+	/* cart page */
+	// title 길이 자르기
+	$('.cart-item-title span').text(function (_, text) {
+		return text.length > 50 ? text.substr(0, 50) + '...' : text;
+	});
+	// 삭제버튼 이벤트
+	$('.selboxDirect').hide();
+	$('#selbox').on('change', function () {
+		if ($('#selbox').val() == 'direct') {
+			$('.selboxDirect').show();
+		} else {
+			$('.selboxDirect').hide();
+		}
+
+		// month, date 연결
+		var month = new Date().getMonth() + 1;
+		var date = new Date().getDay() + 1;
+
+		$('.cart-item-info .delivery-date b').eq(0).text(month);
+		$('.cart-item-info .delivery-date b').eq(1).text(date);
+	});
+
+	// Cart items 삭제버튼
+	$('.cart-items-header .btn-all-clear').on('click', function () {
+		$('.cart-item').hide();
+		$('.cart-items-summary .price').text('0원');
+		$('.cart-items').html(
+			'<p class="no-item">장바구니에 담긴 상품이 없습니다.</p>'
+		);
+	});
+	$('.cart-item-title .btn-clear').on('click', function () {
+		console.log('click');
+		$(this).parent().parent().hide();
+	});
+
+	// cart-item의 수량버튼
+	$('.count-wrap button').on('click', function (e) {
+		e.preventDefault();
+		var $count = $(this).parent('.count-wrap').find('.input');
+		var now = parseInt($count.val());
+		var min = 1;
+		var max = 99;
+		var num = now;
+
+		if ($(this).hasClass('minus')) {
+			if (now > min) num = now - 1;
+		} else {
+			if (now < max) num = now + 1;
+		}
+		if (num != now) $count.val(num);
 	});
 });
